e91722cca0774842bd361c48bf3dfd5c303e2f29
tanubordia <tb24697@gmail.com>
2020-04-04 23:22:03 +0530

    Add files via upload

---
+++ LoginController.java
@@ -0,0 +1,55 @@
+
+package com.acms.CentralSellerPortal.Controllers;
+
+
+import com.acms.CentralSellerPortal.Entities.Seller;
+import com.acms.CentralSellerPortal.Repositories.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+
+@RestController
+public class LoginController {
+
+
+
+
+
+        @Autowired
+        SellerRepository sellerRepository;
+        @Autowired
+        CompanyRepository companyRepository;
+
+        @GetMapping("/seller/{seller_id}")
+
+
+        @GetMapping("/seller")
+      public Seller create(@RequestBody Map<String,String> body){
+
+        List<sellerRepository> seller = sellerRepository.findAll(body.get("email")).orElse(null);
+        for(seller s:list){
+          if(body.get("password").equals(s.getPassword())){
+
+
+          return s;
+      }
+
+    }
+    return null;
+  }
+    @GetMapping("/company")
+  public Company create(@RequestBody Map<String,String> body){
+
+    List<companyRepository> company = companyRepository.findAll(body.get("email")).orElse(null);
+    for(company c:list){
+      if(body.get("password").equals(c.getPassword())){
+
+
+      return c;
+  }
+}
+  return null;
+}
+}
