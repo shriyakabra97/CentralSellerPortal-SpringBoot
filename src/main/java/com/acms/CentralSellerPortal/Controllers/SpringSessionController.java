82440ecfaafb29baa01203e6dbc1954b861e3b01
Anmol Tuteja <Anmol.Tuteja@iiitb.org>
2020-04-19 16:00:51 +0530

    Session Controller with api for destroying session at log out

--- src/main/java/com/acms/CentralSellerPortal/Controllers/SpringSessionController.java
+++ src/main/java/com/acms/CentralSellerPortal/Controllers/SpringSessionController.java
@@ -1,4 +1,24 @@
+/* author Anmol Tuteja
+Session Controller has api for destroying the session while logging out
+ */
+
 package com.acms.CentralSellerPortal.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
 public class SpringSessionController {
+
+    @RequestMapping(value="/destroy" )
+        public RedirectView destroyingSession(HttpSession session)
+    {
+        session.invalidate();
+        return new RedirectView("/");
+    }
+
+
 }
